151005058da89bf7b60d409c3abd3e1ac150bd05
WingCH <wingch.apps@gmail.com>
2021-10-08 11:04:58 +0800

    Initial Commit

---
+++ TransparentHomeWidget-/TransparentHomeWidget_App.swift
@@ -0,0 +1,17 @@
+//
+//  TransparentHomeWidget_App.swift
+//  TransparentHomeWidget-
+//
+//  Created by WingCH on 8/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct TransparentHomeWidget_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
