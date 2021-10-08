31b1487a54c74e2a7bef1dcc84c836304f0b76ab
WingCH <wingch.apps@gmail.com>
2021-10-08 11:07:15 +0800

    init ContentViewModel

--- TransparentHomeWidget-/ContentView.swift
+++ TransparentHomeWidget-/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel: ContentViewModel = ContentViewModel()
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
---
+++ TransparentHomeWidget-/ContentViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentViewModel.swift
+//  TransparentHomeWidget-
+//
+//  Created by WingCH on 8/10/2021.
+//
+
+import Foundation
+
+class ContentViewModel: ObservableObject {
+    init(){
+        
+    }
+}
